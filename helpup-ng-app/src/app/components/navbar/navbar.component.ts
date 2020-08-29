c3fb2f63ef1d22b52621abd6238bec21910b1bae
malinabenegui <malina.benegui@gmail.com>
2020-08-29 12:50:35 +0300

    Register Error message styled

--- helpup-ng-app/src/app/components/navbar/navbar.component.ts
+++ helpup-ng-app/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public cookies: CookieService) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.cookies.delete('jwt');
+  }
 }
